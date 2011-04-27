8ab5341765e7ab2cf4eb73569764f2f5f2c3598a
Andrew Odewahn <odewahn@oreilly.com>
2011-04-27 09:16:40 -0700

    Committing intital code

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Define the title, data sources, and various data entry spreadsheets
+$config = array (
+   "site_title" => "Maker Press Authoring Tool",
+   "logo_url" => "logo.png",
+   "base_url" => "http://127.0.0.1/~odewahn/maker_press/content/"
+);
+?>
+
